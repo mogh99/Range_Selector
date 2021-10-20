78927dd2b059fa768de51919eb5bfa7608346378
mogh99 <mahk1092@gmail.com>
2021-10-20 18:08:37 +0300

    Update the main

--- RangeSelector/src/main.cpp
+++ RangeSelector/src/main.cpp
@@ -26,26 +26,6 @@ static void glfw_error_callback(int error, const char* description)
 	fprintf(stderr, "Glfw Error %d: %s\n", error, description);
 }
 
-ifd::FileDialog::Instance().CreateTexture = [](uint8_t* data, int w, int h, char fmt) -> void* {
-	GLuint tex;
-
-	glGenTextures(1, &tex);
-	glBindTexture(GL_TEXTURE_2D, tex);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, (fmt == 0) ? GL_BGRA : GL_RGBA, GL_UNSIGNED_BYTE, data);
-	glGenerateMipmap(GL_TEXTURE_2D);
-	glBindTexture(GL_TEXTURE_2D, 0);
-
-	return (void*)tex;
-};
-ifd::FileDialog::Instance().DeleteTexture = [](void* tex) {
-	GLuint texID = (GLuint)tex;
-	glDeleteTextures(1, &texID);
-};
-
 int main() {
 	// Set the reading and writing precision to 
 	std::cout.precision(DOUBLE_PRECISION);
